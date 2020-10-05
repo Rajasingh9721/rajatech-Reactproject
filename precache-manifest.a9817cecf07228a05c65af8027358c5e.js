f6f2972593843f01b608035c7bb636396defc305
Raja Singh <rajasingh9721@gmail.com>
2020-10-05 05:57:27 +0530

    Updates

---
+++ precache-manifest.a9817cecf07228a05c65af8027358c5e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7bf566d13c1423fafce290affb93d9",
+    "url": "/rajatech-Reactproject/index.html"
+  },
+  {
+    "revision": "a16ad06300fdf37dc4ec",
+    "url": "/rajatech-Reactproject/static/css/2.383bd4f4.chunk.css"
+  },
+  {
+    "revision": "d9555db185cf494c14cc",
+    "url": "/rajatech-Reactproject/static/css/main.c3dcf927.chunk.css"
+  },
+  {
+    "revision": "a16ad06300fdf37dc4ec",
+    "url": "/rajatech-Reactproject/static/js/2.7e6f958b.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/rajatech-Reactproject/static/js/2.7e6f958b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9555db185cf494c14cc",
+    "url": "/rajatech-Reactproject/static/js/main.f12c2404.chunk.js"
+  },
+  {
+    "revision": "fe8c1d97c8bcf0285678",
+    "url": "/rajatech-Reactproject/static/js/runtime-main.d71ceca9.js"
+  },
+  {
+    "revision": "8c55055762438f525f2af8372014dbc6",
+    "url": "/rajatech-Reactproject/static/media/1.8c550557.png"
+  },
+  {
+    "revision": "e5364f6f04b0bc4f863b5c1d8ea86b66",
+    "url": "/rajatech-Reactproject/static/media/2.e5364f6f.png"
+  },
+  {
+    "revision": "027417ef0c2aa6344a9cb82f31ae9c50",
+    "url": "/rajatech-Reactproject/static/media/android.027417ef.jfif"
+  },
+  {
+    "revision": "2d3e6fd59a431cc229e51e154ed32843",
+    "url": "/rajatech-Reactproject/static/media/app.2d3e6fd5.jfif"
+  },
+  {
+    "revision": "81a9c8f96cef5ebdca374a5a1c1a0532",
+    "url": "/rajatech-Reactproject/static/media/digital.81a9c8f9.jfif"
+  },
+  {
+    "revision": "422347ea3b443cd090129e033efe96d1",
+    "url": "/rajatech-Reactproject/static/media/marketing.422347ea.jfif"
+  },
+  {
+    "revision": "5a11fb77ca92c53292022c6da595b533",
+    "url": "/rajatech-Reactproject/static/media/software.5a11fb77.jfif"
+  },
+  {
+    "revision": "a4c0249e1e3ae9308654394ea2c7cf2e",
+    "url": "/rajatech-Reactproject/static/media/web.a4c0249e.jfif"
+  }
+]);
